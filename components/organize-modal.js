c1723eea7c1d2e3e664d69a4c411f7c60961ed8b
PlastikMaykr <129111623+PlastikMaykr@users.noreply.github.com>
2024-08-06 22:43:38 +0200

    organize modal - rudimentary swatch dragging

--- components/organize-modal.js
+++ components/organize-modal.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3';
 import './modal.css';
+import { Swatch, Palette } from '../classes';
 
 export class OrganizeModal {
     static { }
@@ -36,7 +37,9 @@ export class OrganizeModal {
         this.openButton.on('click', () => this.open());
 
         this.modal = d3.select(dialog).select('.modal');
-        this.overview = this.modal.select('.overview');
+        this.overview = this.modal.select('.overview')
+            .call(attachDragHandle)
+            .call(dragger, this);
 
         this.callback = callback || (() => { });
     }
@@ -68,6 +71,29 @@ export class OrganizeModal {
         console.log('Done organizing');
 
         this.overview.selectAll('div').remove();
+
+        this.callback();
+    }
+
+    update() {
+        this.overview.selectAll('div').remove();
+
+        this.overview.selectAll('div')
+            .data(Palette.collection)
+            .join('div')
+            .classed('org-wrapper', true)
+            .call(wrapper => wrapper
+                .append('p')
+                .call(editableContent, 'name')
+            )
+            .append('div')
+            .classed('org-palette', true)
+            .selectAll('div')
+            .data(d => d.swatches)
+            .join('div')
+            .classed('org-swatch', true)
+            .style('background-color', d => d.color.formatHex())
+            .call(editableContent, 'name');
 
         this.callback();
     }
@@ -110,7 +136,7 @@ function editableContent(element, property) {
             }
         })
         .on('focus', function () {
-            console.log('focus', this);
+            // console.log('focus', this);
             /* 
             const selection = d3.select(this);
             const datum = selection.datum();
@@ -119,7 +145,7 @@ function editableContent(element, property) {
              */
         })
         .on('blur', function () {
-            console.log('blur', this);
+            // console.log('blur', this);
 
             const selection = d3.select(this);
             selection.attr('contenteditable', 'false');
@@ -129,4 +155,110 @@ function editableContent(element, property) {
             datum[property] = text;
             // console.log({ datum, text });
         });
+}
+
+let dragActive = false;
+let dropZone = null;
+
+const dragHandle = d3.create('div')
+    .classed('drag-handle', true)
+    .text('⋮')
+    .node();
+
+/** @param {d3.Selection} element */
+function attachDragHandle(element) {
+    element
+        .on('mouseover', function (event) {
+            // console.log(event);
+            // const targetCondition = event.target.getAttribute('contenteditable') === 'false';
+            const targetCondition = event.target.matches('.org-swatch');
+            if (targetCondition) {
+                if (dragActive) {
+                    dropZone = event.target;
+                } else {
+                    event.target.append(dragHandle)
+                }
+            } else {
+                dragHandle.remove();
+                dropZone = null;
+            }
+        })
+}
+
+function dragger(overview, organizeModal) {
+    /** @type {d3.Selection<HTMLDivElement, Swatch>} */
+    let dragged = null;
+
+    const bucket = d3.create('div')
+        .classed('bucket', true)
+
+    /** @param {d3.D3DragEvent} event */
+    function dragstart(event) {
+        console.log('dragStart', event);
+
+        dragActive = true;
+
+        dragged = d3.select(event.sourceEvent.target.parentNode)
+            .style('display', 'none');
+
+        const [x, y] = d3.pointer(event, this);
+
+        this.append(bucket
+            .style('left', x + 'px')
+            .style('top', y + 'px')
+            .style('background-color', dragged.style('background-color'))
+            .node());
+    }
+
+    /** @param {d3.D3DragEvent} event */
+    function dragging(event) {
+        // console.log(event);
+        const [x, y] = d3.pointer(event, this);
+
+        bucket
+            .style('left', x + 'px')
+            .style('top', y + 'px')
+            .style('background-color', dragged.style('background-color'));
+    }
+
+    /** @param {d3.D3DragEvent} event */
+    function dragend(event) {
+        console.log('dragEnd', event);
+
+        dragged.style('display', null);
+        bucket.remove();
+
+        if (dropZone) {
+            const dragSwatch = dragged.datum();
+
+            /** @type {Swatch} */
+            const dropSwatch = d3.select(dropZone).datum();
+            dragSwatch.palette.swatches.splice(dragSwatch.getIndex(), 1);
+            dragSwatch.palette = dropSwatch.palette;
+            const index = dropSwatch.getIndex();
+            dropSwatch.palette.swatches.splice(index, 0, dragSwatch);
+
+            organizeModal.update();
+        }
+
+        dragged = null;
+
+        dragActive = false;
+    }
+
+    return d3.drag()
+        .filter(function (event) {
+            // console.log(event);
+
+            return (
+                !event.ctrlKey &&
+                !event.button &&
+                event.originalTarget === dragHandle
+                // event.originalTarget.getAttribute('contenteditable') === 'false'
+            );
+        })
+        .on('start', dragstart)
+        .on('drag', dragging)
+        .on('end', dragend)
+        (overview);
 }
